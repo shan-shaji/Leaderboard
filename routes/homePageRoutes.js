81bfb406944c7edf14edc2bb3480f218f0e9ad8f
tonalmathew <tonalmathew1999@gmail.com>
2020-08-03 21:20:37 +0530

    updated style.css

--- routes/homePageRoutes.js
+++ routes/homePageRoutes.js
@@ -10,11 +10,14 @@ var result;
 // home Page route
 router.get("/", (req, res) => {
   if (datafetched == true) {
-    res.render("pages/home", { rankingResult: result });
+    res.render("pages/home", { rankingResult: result, result1: result1, result2: result2, result3: result3 });
   }
 
   axios.get(leaderBoardUrl).then(
     (response) => {
+      result1 = response.data[0];
+      result2 = response.data[1];
+      result3 = response.data[2];
       result = response.data;
       datafetched = true;
       res.redirect("/");
