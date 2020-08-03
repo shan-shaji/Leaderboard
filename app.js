d62f64cd9635994da5eed56c154f859fe7f2b95c
shan-shaji <shanshaji224@gmail.com>
2020-08-03 16:05:02 +0530

    changed project to api based

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const homePageRoutes = require("./routes/homePageRoutes.js");
-const loginRoutes = require("./routes/login.js");
+
 const ejs = require("ejs");
-const InitiateMongoServer = require("./config/db.js");
 
 // initiate mongo server
 InitiateMongoServer();
@@ -19,12 +18,9 @@ app.use(express.static("public"));
 
 // setting Templating Engine
 app.set("view engine", "ejs");
-// bodyParser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Grouping route handlers
 app.use("/", homePageRoutes);
-app.use("/login", loginRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
